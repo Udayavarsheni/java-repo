b27862a8c0d7f091b6b77e7a79ef1d56a59f9239
Udayavarsheni <udayavarsheni@gmail.com>
2019-07-05 18:20:58 +0530

    tcast commit

---
+++ Tcast.java
@@ -0,0 +1,16 @@
+class Tcast
+{
+public static void main(String[] args)
+{   
+  
+  System.out.println("int converted to double");
+  double b; 
+  b = 70;
+  System.out.println(" b :" + b);
+  System.out.println("double converted to int");
+  int a;
+  a = (int) 12.8;
+  System.out.println("a :" + a);
+  
+ }
+}
