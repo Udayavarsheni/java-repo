e4554354d1a12faf38ffdb7c2853f89f9e9c47c9
Udayavarsheni <udayavarsheni@gmail.com>
2019-07-06 18:34:50 +0530

    throw commit

---
+++ Throw.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Throw {
+  static void checkAge(int age) {
+    if (age < 18) {
+      throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+    }
+    else {
+      System.out.println("Access granted - You are old enough!");
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Enter a value:");
+   Scanner i = new Scanner(System.in); 
+	int a=i.nextInt();     
+        
+    checkAge(a); // Set age to 15 (which is below 18...)
+  }
+}
